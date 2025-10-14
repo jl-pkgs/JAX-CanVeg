772199bb0076b8500f5db3123e6be6aa0acd01ed
Dongdong Kong <kongdd@users.noreply.github.com>
2025-10-14 23:16:18 +0800

    add tests for 3 model types

---
+++ tests/train_testmodel.py
@@ -0,0 +1,25 @@
+"""Train the models."""
+
+from os import path
+from pathlib import Path
+
+import jax
+from jax_canveg import train_model
+from jax_canveg.shared_utilities import tune_jax_naninfs_for_debug
+
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_traceback_filtering", "off")
+tune_jax_naninfs_for_debug(False)
+
+################################################################
+# Run the model
+################################################################
+
+def getwd(): return Path(path.abspath(path.dirname(__file__)))
+
+
+f_Hybrid = getwd() / "Hybrid-1L-0.0/configs.json" # US-Whs
+f_PB = getwd() / "PB-1L-0.0/configs.json" # US-Whs
+
+train_model(f_Hybrid, save_log_local=True)  # pyright: ignore
+train_model(f_PB, save_log_local=True)  # pyright: ignore
