3ed66a2accb7e5eabe1e75a9d7458eac04ad761a
Dongdong Kong <kongdd@users.noreply.github.com>
2025-10-18 23:13:22 +0800

    minor

--- src/jax_canveg/shared_utilities/solver/fixed_point.py
+++ src/jax_canveg/shared_utilities/solver/fixed_point.py
@@ -1,39 +1,35 @@
 import jax
 from jax import jvp, jacfwd
-
-# import jax.numpy as jnp
 import equinox as eqx
 import lineax as lx
-
-
 from typing import Callable, List
-
-# from ...shared_utilities.types import Float_2D
-# from functools import partial
-
 from ...subjects import Para
 
+# import jax.numpy as jnp
+# from ...shared_utilities.types import Float_2D
+# from functools import partial
 # from ...subjects import Para, Lai, ParNir, LeafAng
 # from ...subjects import Met, Prof, SunAng, SunShadedCan
 # from ...subjects import Veg, Soil, Qin, Ir, Can
 
 
+# states_initial: list[
+#     Met,
+#     Prof,
+#     Ir,
+#     Qin,
+#     SunAng,
+#     SunShadedCan,
+#     SunShadedCan,
+#     Soil,
+#     Veg,
+#     Can,
+# ],
+
 @eqx.filter_jit
 def fixed_point(
     func: Callable,
     states_initial: List,
-    # states_initial: list[
-    #     Met,
-    #     Prof,
-    #     Ir,
-    #     Qin,
-    #     SunAng,
-    #     SunShadedCan,
-    #     SunShadedCan,
-    #     Soil,
-    #     Veg,
-    #     Can,
-    # ],
     para: Para,
     niter: int,
     *args,
